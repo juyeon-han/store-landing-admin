bfe2e9f1a47940b73fd7cbc09810efdba61962b0
한주연 <juyeon.han@yaksonhealthcare.com>
2025-03-25 11:52:10 +0900

    feat: BreadCrumb 컴포넌트 생성

---
+++ src/components/breadcrumb/BreadCrumb.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { Breadcrumb as AntdBreadcrumb } from 'antd';
+import classNames from 'classnames/bind';
+import { pathLabel } from '@/routes';
+import styles from './BreadCrumb.module.scss';
+
+interface BreadCrumbProps {}
+
+const BreadCrumb = (props: BreadCrumbProps) => {
+  const cx = classNames.bind(styles);
+  const [paths, setPaths] = useState<string[]>([]);
+
+  const getPathLabel = (path: string) => {
+    return pathLabel[decodeURIComponent(path)] || '알 수 없는 페이지';
+  };
+
+  useEffect(() => {
+    const pathSnippets = location.pathname.split('/').filter((i) => i);
+    setPaths(pathSnippets);
+  }, [location.pathname]);
+
+  return (
+    <AntdBreadcrumb
+      className={cx('breadcrumb')}
+      items={paths.map((snippet) => ({
+        title: getPathLabel(snippet),
+      }))}
+    />
+  );
+};
+
+export default BreadCrumb;
